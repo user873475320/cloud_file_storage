729e9d8d5c4ef462cd7655dbf98bb86571142fd7
Artem Pestrikov <user873475320@gmail.com>
2024-09-16 13:44:13 +0500

    refactor: add logging, work with custom exceptions and change return type of 'save' method

--- src/main/java/ru/storage/service/UserService.java
+++ src/main/java/ru/storage/service/UserService.java
@@ -1,24 +1,44 @@
 package ru.storage.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.storage.dto.UserRegistrationDTO;
+import ru.storage.dto.UserDTO;
 import ru.storage.entity.User;
+import ru.storage.exception.RepositoryException;
+import ru.storage.exception.UsernameAlreadyExistException;
 import ru.storage.repository.UserRepository;
 
-@Service
+@Slf4j
 @RequiredArgsConstructor
+@Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void save(UserRegistrationDTO userDTO) {
-        userRepository.save(User.builder()
-                        .username(userDTO.getUsername())
-                        .password(passwordEncoder.encode(userDTO.getPassword()))
-                .build());
+    public boolean save(UserDTO userDTO) {
+        if (userDTO == null) {
+            log.error("Failed to save user: null provided");
+            return false;
+        }
+
+        try {
+            User savedUser = userRepository.save(User.builder()
+                    .username(userDTO.getUsername())
+                    .password(passwordEncoder.encode(userDTO.getPassword()))
+                    .build());
+            log.debug("The next user was saved: {}", savedUser);
+        } catch (DataIntegrityViolationException e) {
+            log.warn("User with the next username already exist: {}", userDTO.getUsername());
+            throw new UsernameAlreadyExistException(e);
+        } catch (Exception e) {
+            log.error("Unexpected exception in UserRepository in 'save' method");
+            throw new RepositoryException("Exception in UserRepository 'save' method", e);
+        }
+        return true;
     }
 }
