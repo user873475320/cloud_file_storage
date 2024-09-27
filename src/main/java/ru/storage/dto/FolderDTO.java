8546d6b72494355787d0249251c1cac86d1db8cd
Artem Pestrikov <user873475320@gmail.com>
2024-09-27 15:51:50 +0500

    feat: add @Builder annotation

--- src/main/java/ru/storage/dto/FolderDTO.java
+++ src/main/java/ru/storage/dto/FolderDTO.java
@@ -1,6 +1,7 @@
 package ru.storage.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class FolderDTO {
